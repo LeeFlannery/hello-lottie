da7b219dd84a054650eea0e136960b9c93f825d7
Lee Flannery <ljflannery@gmail.com>
2021-04-30 10:49:36 -0400

    Add Lottie to project

--- hello-lottie/hello-lottie/ViewController.swift
+++ hello-lottie/hello-lottie/ViewController.swift
@@ -1,17 +1,39 @@
 //
 //  ViewController.swift
-//  hello-lottie
+//  ohailottie
 //
-//  Created by Lee Flannery on 4/22/21.
+//  Created by Lee Flannery on 4/17/21.
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    let animationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let animation = Animation.named("success-animation", subdirectory: "LottieAnimations")
+        
+        animationView.animation = animation
+        // animationView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        animationView.contentMode = .scaleAspectFit
+        view.addSubview(animationView)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        animationView.backgroundBehavior = .pauseAndRestore
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        animationView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        animationView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        animationView.setContentCompressionResistancePriority(.fittingSizeLevel, for: .horizontal)
+        
+        animationView.play()
     }
 
 
